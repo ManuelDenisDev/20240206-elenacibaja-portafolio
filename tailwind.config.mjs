e9ec8f25a48e64160ea924fdbae06161d7463a58
ManuelDenisDev <manueldenis.developer@gmail.com>
2024-02-07 01:26:10 +0100

    final primer dia

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				marcellus: ['"Marcellus"', 'sans-serif'],
+				grandslang: ['"Grand Slang"', 'sans-serif'],
+			},
+			colors: {
+				main: '#262626',
+				secondary: '#d9d6c7',
+				tertiary: '#f2dad8',
+				secondaryd: '#a69286',
+				tertiaryd: '#bf9793',
+			},
+		},
 	},
 	plugins: [],
-}
+};
